d635d77a02c078f6c2dada863e5eb441b50e3aee
isuyun <sylee@islab.snu.ac.kr>
2025-05-16 18:56:46 +0900

    [FIX Path] pickle_view_convt.py

--- pickle_view_convt.py
+++ pickle_view_convt.py
@@ -1,24 +1,43 @@
+
+
+# for i in range(1, 21):
+#     with open(f"/sdb1/piper_subtask_data/train/pick/Pick the blue plastic cup in the center./{i}/episode.pickle", "rb") as f:
+#         data = pickle.load(f)
+#
+#     # 전체 구조 보기 (요약)
+#     # for key in data:
+#     #     print(f"{key}: type={type(data[key])}, shape={getattr(data[key], 'shape', None)}")
+#
+#
+#     outdir = f"/sdb1/piper_subtask_data/train/pick/Pick the blue plastic cup in the center./downnp/{i}"
+#     os.makedirs(outdir, exist_ok=True)
+#     np.savez(f"{outdir}/episode.npz", **data)
+
 import pickle
 import numpy as np
+import pandas as pd
 import os
+
+def to_numpy(x):
+    return x.to_numpy() if isinstance(x, pd.Series) else np.array(x)
 
 for i in range(1, 21):
     with open(f"/sdb1/piper_subtask_data/train/pick/Pick the blue plastic cup in the center./{i}/episode.pickle", "rb") as f:
         data = pickle.load(f)
 
-    # 전체 구조 보기 (요약)
-    # for key in data:
-    #     print(f"{key}: type={type(data[key])}, shape={getattr(data[key], 'shape', None)}")
-
+    clean_data = {
+        'timestamp': to_numpy(data['timestamp']),
+        'frame_index': to_numpy(data['frame_index']),
+        'episode_index': to_numpy(data['episode_index']),
+        'index': to_numpy(data['index']),
+        'task_index': to_numpy(data['task_index']),
+        'action': to_numpy(data['action']),
+        'observation.state': to_numpy(data['observation.state']),
+        'observation.images.wrist': to_numpy(data['observation.images.wrist']),
+        'observation.images.exo': to_numpy(data['observation.images.exo']),
+        'observation.images.table': to_numpy(data['observation.images.table']),
+    }
 
     outdir = f"/sdb1/piper_subtask_data/train/pick/Pick the blue plastic cup in the center./downnp/{i}"
     os.makedirs(outdir, exist_ok=True)
-    np.savez(f"{outdir}/episode.npz", **data)
-# print(data['episode_index'][:10])
-# print(data['index'][:10])
-# print(data['task_index'][:10])
-
-# 예시로 일부 확인
-#print("예시 데이터:")
-#print("첫 번째 이미지 shape:", data['observation.images.table'][0].shape)
-#print("첫 번째 action:", data['action'][0])
+    np.savez(f"{outdir}/episode.npz", **clean_data)
