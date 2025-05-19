31cf61a0bd55f4137801c22c31e667a758151002
isuyun <sylee@islab.snu.ac.kr>
2025-05-19 13:10:44 +0900

    [RUN] finetune.py

---
+++ ft.sh
@@ -0,0 +1,18 @@
+
+CUDA_VISIBLE_DEVICES=1 \
+  torchrun \
+  --standalone --nnodes 1 --nproc-per-node 1 vla-scripts/finetune.py \
+  --vla_path "/sda1/openvla-7b" \
+  --data_root_dir "/home/sylee/tensorflow_datasets" \
+  --dataset_name "piper5_hz_subtask" \
+  --run_root_dir "/sdc1/piper_subtask/openvla" \
+  --adapter_tmp_dir "/sdc1/piper_subtask/openvla/tmp" \
+  --lora_rank 32 \
+  --batch_size 4 \
+  --grad_accumulation_steps 1 \
+  --learning_rate 5e-4 \
+  --image_aug True \
+  --wandb_project "openvla" \
+  --wandb_entity "suyunlee" \
+  --save_steps 1 \
+  --max_steps 1
