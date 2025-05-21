f2b60ebcaa7191f6bf5353c21ad789a69560694e
isuyun <sylee@islab.snu.ac.kr>
2025-05-21 18:21:00 +0900

    [RUN] finetune_gripper.py

--- Eval_plot.py
+++ Eval_plot.py
@@ -145,10 +145,10 @@ if __name__ == "__main__":
         g.append(data['action'][i][0][6])
         g1.append(data1['action'][i][0][6])
 
-    print("Top 10 max values:", sorted(g, reverse=True)[:10])
-    print("Top 10 min values:", sorted(g)[:10])
-    print("Top 10 max values:", sorted(g1, reverse=True)[:10])
-    print("Top 10 min values:", sorted(g1)[:10])
+    print("Top 10 max values:", sorted(g, reverse=True)[10:20])
+    print("Top 10 min values:", sorted(g)[10:20])
+    print("Top 10 max values:", sorted(g1, reverse=True)[10:20])
+    print("Top 10 min values:", sorted(g1)[10:20])
     exit()
 
 
