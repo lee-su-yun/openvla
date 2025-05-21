ab20af8ba7aebce874158639c2510896ce352b28
isuyun <sylee@islab.snu.ac.kr>
2025-05-21 13:39:46 +0900

    [RUN] finetune_with_val.py

--- vla-scripts/finetune_with_val.py
+++ vla-scripts/finetune_with_val.py
@@ -467,7 +467,7 @@ def finetune(cfg: FinetuneConfig) -> None:
                 # === Run Validation after Checkpoint ===
             # if distributed_state.is_main_process and gradient_step_idx % val_every_n_steps == 0:
             #     val_loss, val_acc, val_l1 = evaluate(vla, val_dataloader, device_id, action_tokenizer)
-            if True and gradient_step_idx % val_every_n_steps == 0:
+            if (gradient_step_idx!=0) and (gradient_step_idx% val_every_n_steps == 0):
                 val_loss, val_acc, val_l1 = evaluate(vla, val_dataloader, device, action_tokenizer)
                 wandb.log(
                     {
