88b7b05894c9438c4ad719df9afba83d2252fd91
isuyun <sylee@islab.snu.ac.kr>
2025-05-22 17:53:15 +0900

    [RUN] Eval_plot.py

--- Eval_plot.py
+++ Eval_plot.py
@@ -60,7 +60,7 @@ if __name__ == "__main__":
 
     # Load Processor & VLA
     #model_path = "/sdc1/piper_subtask/openvla/openvla-7b+piper5_hz_subtask+b16+lr-0.0005+lora-r32+dropout-0.0--image_aug"
-    model_path = "/sdc1/piper_subtask/openvla/Norm/openvla-7b+piper5_hz_subtask+b16+lr-0.0005+val+lora-r32+dropout-0.0--image_aug+norm/step_1000"
+    model_path = "/sdc1/piper_subtask/openvla/Norm/openvla-7b+piper5_hz_subtask+b16+lr-0.0005+val+lora-r32+dropout-0.0--image_aug+norm/step_2000"
     processor = AutoProcessor.from_pretrained(model_path, trust_remote_code=True)
     vla = AutoModelForVision2Seq.from_pretrained(
         #"/sdb1/ckpt/openvla_5hz_n/openvla-7b+piper5_hz+b16+lr-0.0005+lora-r32+dropout-0.0/latest",
@@ -258,7 +258,7 @@ if __name__ == "__main__":
 
 
     #with open("/sdb1/piper_subtask_data/eval/pick/Validation/Pick the blue cup on the right./episode.pickle", "rb") as f:
-    with open("/sdb1/piper_subtask_data/eval/pick/Val_np/Pick the yellow cup./episode.pickle", "rb") as f:
+    with open("/sdb1/piper_subtask_data/eval/pick/Val_np/Pick the blue cup on the right./episode.pickle", "rb") as f:
         data = pickle.load(f)
     #
     # with open("/sdb1/piper_subtask_data/eval/pick/Val_np/Pick the yellow cup./episode.pickle", "rb") as f:
@@ -281,7 +281,7 @@ if __name__ == "__main__":
     #for i in range(50):
     for i in range(0, 300, 6):
         image = Image.fromarray(data['observation.images.table'][i][0])
-        prompt = "In: What should the robot do to pick the yellow cup?\nOut:"
+        prompt = "In: What should the robot do to pick the blue cup on the right?\nOut:"
         inputs = processor(prompt, image).to(device, dtype=torch.bfloat16)
         action = vla.predict_action(**inputs, unnorm_key="piper5_hz", do_sample=False)
         traj_111_latest.append(action)
@@ -368,10 +368,10 @@ if __name__ == "__main__":
         ),
     )])
     fig_111_latest.show()
-    fig_111_latest.write_html("Pick_the_yellow_cup.html")
+    fig_111_latest.write_html("Pick_the_blue_cup_on_the_right.html")
     for i in range(7):
         n = f'71{i + 1}'
         plt.subplot(int(n))
         plt.plot(predictions_111_latest[i], 'b--', gt_111[i], 'r')
-    plt.savefig("Pick_the_yellow_cup.png")
+    plt.savefig("Pick_the_blue_cup_on_the_right.png")
     plt.show()
