79d1ed1f2223c914b599b3a5e1dc67f19c90eae0
isuyun <sylee@islab.snu.ac.kr>
2025-05-23 12:42:50 +0900

    [RUN] Eval_plot.py

--- Eval_plot.py
+++ Eval_plot.py
@@ -60,7 +60,7 @@ if __name__ == "__main__":
 
     # Load Processor & VLA
     #model_path = "/sdc1/piper_subtask/openvla/openvla-7b+piper5_hz_subtask+b16+lr-0.0005+lora-r32+dropout-0.0--image_aug"
-    model_path = "/sdc1/piper_subtask/openvla/Norm/openvla-7b+piper5_hz_subtask+b16+lr-0.0005+val+lora-r32+dropout-0.0--image_aug+norm/step_5000"
+    model_path = "/sdc1/piper_subtask/openvla/Norm/openvla-7b+piper5_hz_subtask+b16+lr-0.0005+val+lora-r32+dropout-0.0--image_aug+norm/step_3000"
     processor = AutoProcessor.from_pretrained(model_path, trust_remote_code=True)
     vla = AutoModelForVision2Seq.from_pretrained(
         #"/sdb1/ckpt/openvla_5hz_n/openvla-7b+piper5_hz+b16+lr-0.0005+lora-r32+dropout-0.0/latest",
