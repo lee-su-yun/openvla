8f402dea8b0f2740c1b6d6a4cbbb560067b9bd54
isuyun <sylee@islab.snu.ac.kr>
2025-05-23 12:31:43 +0900

    [RUN] Eval_plot.py

--- Eval_plot.py
+++ Eval_plot.py
@@ -258,7 +258,11 @@ if __name__ == "__main__":
 
 
     #with open("/sdb1/piper_subtask_data/eval/pick/Validation/Pick the blue cup on the right./episode.pickle", "rb") as f:
-    with open("/sdb1/piper_subtask_data/eval/pick/Val_np/Pick the yellow cup./episode.pickle", "rb") as f:
+    #Pick the blue cup on the right.
+    #Pick the white cup nearest from the robot.
+    #Pick the red cup behind the purple one.
+    #Pick the yellow cup.
+    with open("/sdb1/piper_subtask_data/eval/pick/Val_np/Pick the blue cup on the right./episode.pickle", "rb") as f:
         data = pickle.load(f)
     #
     # with open("/sdb1/piper_subtask_data/eval/pick/Val_np/Pick the yellow cup./episode.pickle", "rb") as f:
@@ -281,7 +285,7 @@ if __name__ == "__main__":
     #for i in range(50):
     for i in range(0, 300, 6):
         image = Image.fromarray(data['observation.images.table'][i][0])
-        prompt = "In: What should the robot do to pick the yellow cup?\nOut:"
+        prompt = "In: What should the robot do to pick the blue cup on the right?\nOut:"
         inputs = processor(prompt, image).to(device, dtype=torch.bfloat16)
         action = vla.predict_action(**inputs, unnorm_key="piper5_hz", do_sample=False)
         traj_111_latest.append(action)
@@ -368,10 +372,14 @@ if __name__ == "__main__":
         ),
     )])
     fig_111_latest.show()
-    fig_111_latest.write_html("Pick_the_yellow_cup.html")
+    fig_111_latest.write_html("Pick_the_blue_cup_on_the_right.html")
+    #Pick_the_blue_cup_on_the_right
+    #Pick_the_white_cup_nearest_from_the_robot
+    #Pick_the_red_cup_behind_the_purple_one
+    #Pick_the_yellow_cup
     for i in range(7):
         n = f'71{i + 1}'
         plt.subplot(int(n))
         plt.plot(predictions_111_latest[i], 'b--', gt_111[i], 'r')
-    plt.savefig("Pick_the_yellow_cup.png")
+    plt.savefig("Pick_the_blue_cup_on_the_right.png")
     plt.show()
