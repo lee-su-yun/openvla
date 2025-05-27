f96b5cbabb7b49c08cfce578b5980a7128f987bb
isuyun <sylee@islab.snu.ac.kr>
2025-05-27 12:43:44 +0900

    [RUN] Eval_plot.py for Top+lora

--- Eval_plot.py
+++ Eval_plot.py
@@ -154,10 +154,12 @@ if __name__ == "__main__":
     gt_111.append(g)
 # 7개의 리스트를 50개의 (7,) 벡터로 전치
     trajectory_array = np.stack(gt_111, axis=1)  # shape: (50, 7)
-
+    print(predictions_111_latest.shape)
+    exit()
     # 저장
-    # np.save("gt_111_latest.npy", trajectory_array)
-    # exit()
+    np.save("gt.npy", trajectory_array)
+    np.save("predict.npy", trajectory_array)
+    exit()
     fig_111_latest = go.Figure(data=[go.Scatter3d(
         x=predictions_111_latest[0], y=predictions_111_latest[1], z=predictions_111_latest[2],
         mode='lines+markers',
