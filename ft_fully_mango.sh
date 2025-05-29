694e3ddf5d7b02b2a2279a0b4a2789930f2d81ae
isuyun <sylee@islab.snu.ac.kr>
2025-05-29 18:20:13 +0900

    [RUN] finetung_fully.py (mango)

---
+++ ft_fully_mango.sh
@@ -0,0 +1,16 @@
+torchrun \
+  --standalone --nnodes 1 --nproc-per-node 1 vla-scripts/finetune_fully.py \
+  --vla_path "/ckpt/openvla-7b" \
+  --data_root_dir "/home/sylee/tensorflow_datasets" \
+  --dataset_name "piper5_hz_subtask" \
+  --run_root_dir "/ckpt/piper_subtask/openvla/Fully" \
+  --batch_size 4 \
+  --grad_accumulation_steps 4 \
+  --learning_rate 5e-4 \
+  --image_aug True \
+  --wandb_project "openvla" \
+  --wandb_entity "suyunlee-seoul-national-university" \
+  --save_steps 1000 \
+  --max_steps 10000 \
+  --use_lora False \
+  --use_quantization False
