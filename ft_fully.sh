80efe12d0f83c938f045cca4ed2d93dfa132bd1a
isuyun <sylee@islab.snu.ac.kr>
2025-05-29 14:02:30 +0900

    [RUN] Eval_plot.py for Top+lora(Train)

---
+++ ft_fully.sh
@@ -0,0 +1,16 @@
+torchrun \
+  --standalone --nnodes 1 --nproc-per-node 1 vla-scripts/finetune_fully.py \
+  --vla_path "/sda1/openvla-7b" \
+  --data_root_dir "/home/sylee/tensorflow_datasets" \
+  --dataset_name "piper5_hz_subtask" \
+  --run_root_dir "/sdc1/piper_subtask/openvla/Fully" \
+  --batch_size 8 \
+  --grad_accumulation_steps 2 \
+  --learning_rate 5e-4 \
+  --image_aug True \
+  --wandb_project "openvla" \
+  --wandb_entity "suyunlee-seoul-national-university" \
+  --save_steps 1000 \
+  --max_steps 10000 \
+  --use_lora False \
+  --use_quantization False
