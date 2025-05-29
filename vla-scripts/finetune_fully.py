88aa64f36da046abef6bb5217060516d259ef9e6
isuyun <sylee@islab.snu.ac.kr>
2025-05-29 14:04:04 +0900

    [RUN] finetung_fully.py

--- vla-scripts/finetune_fully.py
+++ vla-scripts/finetune_fully.py
@@ -186,6 +186,7 @@ def finetune(cfg: FinetuneConfig) -> None:
     if cfg.image_aug:
         exp_id += "--image_aug+norm"
 
+
     # Start =>> Build Directories
     run_dir, adapter_dir = cfg.run_root_dir / exp_id, cfg.adapter_tmp_dir / exp_id
     os.makedirs(run_dir, exist_ok=True)
